5bef7d0e71b8d9cedb587bd85b7b32f6ffc30d49
face2wind <dream.liuguobiao@gmail.com>
2016-07-08 14:31:53 +0800

    mempool testing code

--- testElegance/src/test_mempool.cpp
+++ testElegance/src/test_mempool.cpp
@@ -12,15 +12,23 @@ struct AA
 int main()
 {
 	MemoryPoolManager mp_mgr;
+	int LOOP_TIMES = 1000000;
 
 	long long pre_time = Timer::GetNowTimeMS();
-  for (int i = 0; i < 100000; ++ i)
+  for (int i = 0; i < LOOP_TIMES; ++ i)
   {
-	  //AA *ptr = new AA();
-	  //delete ptr;
-	  AA *ptr = 
+	  AA *ptr = new AA();
+	  delete ptr;
   }
-  cout << "loop time : " << Timer::GetNowTimeMS() - pre_time << " ms" << endl;
+  cout << "new delete loop time : " << Timer::GetNowTimeMS() - pre_time << " ms" << endl;
+
+  pre_time = Timer::GetNowTimeMS();
+  for (int i = 0; i < LOOP_TIMES; ++i)
+  {
+	  AA *ptr = (AA *)mp_mgr.Alloc(sizeof(AA));
+	  mp_mgr.Free(ptr);
+  }
+  cout << "mempool loop time : " << Timer::GetNowTimeMS() - pre_time << " ms" << endl;
 
   return 0;
 }
